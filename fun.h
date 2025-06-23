b551ad4fb0df99056088aa243e835477b4c0356b
AshleyNicoleHP <00078125@uca.edu.sv>
2025-06-22 18:18:41 -0600

    Aplicacion de archivos .h y .c al proyecto para mas orden

---
+++ fun.h
@@ -0,0 +1,26 @@
+#ifndef FUN_H
+#define FUN_H
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <windows.h>
+
+
+// Pantalla de seleccion de pupusa
+void pantallaSeleccion();
+
+//Mapa donde se encuentra el jugador
+void mapaVolcan();
+
+// Batalla contra el Cipitillo
+void batalla();
+
+// Mostrar Torogoz del Bosque en ASCII
+void mostrarTorogozASCII();
+
+// Mostrar Dragon Tlaloc en ASCII
+void mostrarDragonASCII();
+
+#endif
