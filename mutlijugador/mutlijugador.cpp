d1c7dc49e37c5d72f815ccede85670aa5e6908db
Andrewgx123 <00184825@uca.edu.sv>
2025-07-03 14:38:39 -0600

    multijugador

---
+++ mutlijugador/mutlijugador.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+struct Personaje {
+    string nombre;
+    int vida;
+    int maxVida;
+};
+
+struct Mochila {
+    int pociones;
+};
+
+int generarDanio(int min, int max) {
+    return rand() % (max - min + 1) + min;
+}
+
+void mostrarEstado(const Personaje& p1, const Personaje& p2) {
+    cout << "\n======= ESTADO =======\n";
+    cout << p1.nombre << " - Vida: " << p1.vida << endl;
+    cout << p2.nombre << " - Vida: " << p2.vida << endl;
+    cout << "======================\n";
+}
+
